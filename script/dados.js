dbdf70af36521709ac77b47c00bd3b0e12292458
Dennis Matos <milena.msanta@gmail.com>
2024-09-05 03:17:28 -0300

    Primeiro commit

---
+++ script/dados.js
@@ -0,0 +1,24 @@
+let dados = [
+  {
+    titulo: "Cientistas descobrem nova espécie de planta que purifica o ar! ",
+    descricao:
+      "Uma equipe de pesquisadores acaba de identificar uma nova espécie de planta com propriedades incríveis. Essa descoberta representa um grande avanço para a luta contra a poluição do ar e pode trazer benefícios significativos para o meio ambiente. ",
+    imagem: "#",
+    link: "#",
+  },
+  {
+    titulo:
+      "Comunidade planta mil árvores em um dia, criando um novo pulmão para a cidade! ",
+    descricao:
+      "Voluntários de todas as idades se uniram para plantar mil árvores em um parque local, promovendo a recuperação da área verde e melhorando a qualidade do ar. ",
+    imagem: "#",
+    link: "#",
+  },
+  {
+    Nome: "Jovens criam aplicativo para conectar pessoas necessitadas a doações! ",
+    descricao:
+      "Um grupo de estudantes desenvolveu um aplicativo que facilita a doação de alimentos e roupas para quem precisa, combatendo a fome e a desigualdade. ",
+    imagem: "#",
+    link: "#",
+  },
+];
