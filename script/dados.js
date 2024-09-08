f9404bb34779a7df057bd2c34ba30cd8bebabca3
Dennis Matos <milena.msanta@gmail.com>
2024-09-07 23:49:03 -0300

    Aula 5

--- script/dados.js
+++ script/dados.js
@@ -1,5 +1,6 @@
 let dados = [
   {
+    data: "2024-09-07",
     titulo: "Cientistas descobrem nova espécie de planta que purifica o ar! ",
     descricao:
       "Uma equipe de pesquisadores acaba de identificar uma nova espécie de planta com propriedades incríveis. Essa descoberta representa um grande avanço para a luta contra a poluição do ar e pode trazer benefícios significativos para o meio ambiente. ",
@@ -7,18 +8,22 @@ let dados = [
     link: "#",
   },
   {
+    data: "2024-09-06",
     titulo:
       "Comunidade planta mil árvores em um dia, criando um novo pulmão para a cidade! ",
     descricao:
       "Voluntários de todas as idades se uniram para plantar mil árvores em um parque local, promovendo a recuperação da área verde e melhorando a qualidade do ar. ",
-    imagem: "#",
+    imagem: "/Imagens/mil-arvores.jpg",
     link: "#",
   },
   {
+    data: "2024-09-05",
+    titulo:
+      "Jovens criam aplicativo para conectar pessoas necessitadas a doações",
     Nome: "Jovens criam aplicativo para conectar pessoas necessitadas a doações! ",
     descricao:
       "Um grupo de estudantes desenvolveu um aplicativo que facilita a doação de alimentos e roupas para quem precisa, combatendo a fome e a desigualdade. ",
-    imagem: "#",
+    imagem: "/Imagens/pessoas-necessitadas.jpg",
     link: "#",
   },
 ];
--- script/script.js
+++ script/script.js
@@ -1,25 +1,55 @@
 //console.log(dados[1].descricao);
 
-let section = document.getElementById("resultados-pesquisa");
+function pesquisar() {
+  let section = document.getElementById("resultados-pesquisa");
 
-console.log(section);
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-section.innerHTML = `
-         <div class="item-resultado">
-            <img
-              src="${dados[0].imagem}"
-              alt="Imagem da notícia"
-            />
-            <div>
-              <h2>
-                ${dados[0].titulo}
-              </h2>
-              <p class="descricao-meta">
-                ${dados[0].descricao}
-              </p>
-              <a
-                href="${dados[0].link}"
-                target="_blank"
-                >Leia Mais</a
-              ></div>
-         </div>`;
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nenhuma data encontrada!</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase().console.log(campoPesquisa);
+
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    if (
+      dado.data.includes(campoPesquisa) ||
+      dado.titulo.includes(campoPesquisa)
+    ) {
+      resultados += `
+      <div class="item-resultado">
+         <img
+           src="${dado.imagem}"
+           alt="Imagem da notícia"
+         />
+         <div>
+           <h2>
+             ${dado.titulo}
+           </h2>
+           <p class="descricao-meta">
+             ${dado.descricao}
+           </p>
+           <a
+             href="${dado.link}"
+             target="_blank"
+             >Leia Mais</a
+           ></div>
+      </div>`;
+    }
+
+    console.log(dado.data.includes(campoPesquisa));
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>"
+  }
+
+  section.innerHTML = resultados;
+}
